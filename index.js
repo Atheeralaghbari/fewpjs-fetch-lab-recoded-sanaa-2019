8571dddd745275e95f259a543a5ef0c61f2e566d
Atheer Alaghbari <atheeralaghbari61@gmail.com>
2019-11-03 16:50:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,24 +1,46 @@
+// function fetchBooks() {
+// fetch('https://anapioficeandfire.com/api/books')
+// .then(function(response){
+//   return response.json();
+// })
+// .then(function(json){
+//   return renderBooks(json);
+// })
+
+
+
+// function renderBooks(json) {
+//   const main = document.querySelector('main')
+//   json.forEach(book => {
+//     const h2 = document.createElement('h2')
+//     h2.innerHTML = `<h2>${book.name}</h2>`
+//     main.appendChild(h2)
+//   })
+// }
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   fetchBooks()
+// })
+
+
 function fetchBooks() {
-fetch('https://anapioficeandfire.com/api/books')
-.then(function(response){
-  return response.json();
-})
-.then(function(json){
-  return renderBooks(json);
-})
-
-
+    fetch('https://anapioficeandfire.com/api/books')
+      .then(resp => resp.json())
+      .then(json => renderBooks(json));
+      return fetch("https://anapioficeandfire.com/api/books");
+      
+}
 
 function renderBooks(json) {
+  
   const main = document.querySelector('main')
   json.forEach(book => {
     const h2 = document.createElement('h2')
     h2.innerHTML = `<h2>${book.name}</h2>`
-    main.appendChild(h2)
+    main.appendChild(h2) 
   })
 }
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks()
 })
-
