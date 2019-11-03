7ba7c056c6f968173720e542a374f7ab7d32b1fe
Atheer Alaghbari <atheeralaghbari61@gmail.com>
2019-11-03 16:51:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,24 +26,3 @@ document.addEventListener('DOMContentLoaded', function() {
 })
 
 
-function fetchBooks() {
-    fetch('https://anapioficeandfire.com/api/books')
-      .then(resp => resp.json())
-      .then(json => renderBooks(json));
-      return fetch("https://anapioficeandfire.com/api/books");
-      
-}
-
-function renderBooks(json) {
-  
-  const main = document.querySelector('main')
-  json.forEach(book => {
-    const h2 = document.createElement('h2')
-    h2.innerHTML = `<h2>${book.name}</h2>`
-    main.appendChild(h2) 
-  })
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks()
-})
