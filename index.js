5659ca20b6bb860eddcda0aa2ca5a879de8bb607
Atheer Alaghbari <atheeralaghbari61@gmail.com>
2019-11-03 16:51:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
-// function fetchBooks() {
-// fetch('https://anapioficeandfire.com/api/books')
-// .then(function(response){
-//   return response.json();
-// })
-// .then(function(json){
-//   return renderBooks(json);
-// })
+function fetchBooks() {
+fetch('https://anapioficeandfire.com/api/books')
+.then(function(response){
+  return response.json();
+})
+.then(function(json){
+  return renderBooks(json);
+})
+  return fetch("https://anapioficeandfire.com/api/books")
+}
 
 
 
-// function renderBooks(json) {
-//   const main = document.querySelector('main')
-//   json.forEach(book => {
-//     const h2 = document.createElement('h2')
-//     h2.innerHTML = `<h2>${book.name}</h2>`
-//     main.appendChild(h2)
-//   })
-// }
 
-// document.addEventListener('DOMContentLoaded', function() {
-//   fetchBooks()
-// })
+function renderBooks(json) {
+  const main = document.querySelector('main')
+  json.forEach(book => {
+    const h2 = document.createElement('h2')
+    h2.innerHTML = `<h2>${book.name}</h2>`
+    main.appendChild(h2)
+  })
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  fetchBooks()
+})
 
 
 function fetchBooks() {
